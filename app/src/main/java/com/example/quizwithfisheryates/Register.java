68a6bde295e0fd9b47f7be01ae4fa0c7641abd3d
ilhamhafidz404 <xxspanzie@gmail.com>
2025-04-30 22:54:32 +0700

    Register Activity

--- app/src/main/java/com/example/quizwithfisheryates/Register.java
+++ app/src/main/java/com/example/quizwithfisheryates/Register.java
@@ -11,13 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -25,9 +25,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-    public void goToRegister(View view){
-        Intent intent = new Intent(MainActivity.this, Register.class);
+    public void goToLogin(View view){
+        Intent intent = new Intent(Register.this, MainActivity.class);
         startActivity(intent);
     }
 }
